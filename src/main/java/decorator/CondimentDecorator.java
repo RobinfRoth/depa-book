457291fdb4a21746de154c5a221d87fcab994509
Robin Roth <robin.roth1@students.fhnw.ch>
2023-01-23 14:11:48 +0100

    update comment

--- src/main/java/decorator/CondimentDecorator.java
+++ src/main/java/decorator/CondimentDecorator.java
@@ -3,6 +3,6 @@ package decorator;
 // abstract decorator
 
 public abstract class CondimentDecorator extends Beverage {
-    Beverage beverage; // wrapped object
+    Beverage beverage; // wrapped object (composition)
     public abstract String getDescription();
 }
