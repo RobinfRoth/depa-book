a2e79148b1eb5d83110cb67707ee3d9b0f6655a3
Robin Roth <robin.roth1@students.fhnw.ch>
2023-01-25 17:31:39 +0100

    use lambda expressions for concrete commands

---
+++ src/main/java/command/remote/RemoteLoaderLambda.java
@@ -0,0 +1,42 @@
+package command.remote;
+
+public class RemoteLoaderLambda {
+
+    public static void main(String[] args) {
+
+        // invoker
+        RemoteControl remote = new RemoteControl();
+
+        // Receivers
+        Light livingRoomLight = new Light("Living Room");
+        Light kitchenLight = new Light("Kitchen");
+        GarageDoor garageDoor = new GarageDoor("Garage");
+        Stereo livingRoomStereo = new Stereo("Living Room");
+
+
+        // set commands in invoker (remote)
+        remote.setCommand(0, () -> livingRoomLight.on(), () -> livingRoomLight.off());
+        remote.setCommand(1, () -> kitchenLight.on(), () -> kitchenLight.off());
+        remote.setCommand(2, () -> garageDoor.up(), () -> garageDoor.down());
+        remote.setCommand(3,
+                () -> {
+                    livingRoomStereo.on();
+                    livingRoomStereo.setCd("Wish You Were Here");
+                    livingRoomStereo.setVolume(42);
+                },
+                () -> livingRoomStereo.off());
+
+        // print remote
+        System.out.println(remote);
+
+        // press buttons on remote (invoker runs execute() )
+        remote.onButtonWasPushed(0);
+        remote.offButtonWasPushed(0);
+        remote.onButtonWasPushed(1);
+        remote.offButtonWasPushed(1);
+        remote.onButtonWasPushed(2);
+        remote.offButtonWasPushed(2);
+        remote.onButtonWasPushed(3);
+        remote.offButtonWasPushed(3);
+    }
+}
